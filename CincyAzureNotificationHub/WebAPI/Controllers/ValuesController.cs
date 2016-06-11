f99965a861c02999cba65ccd31416cfd13397c3b
Dave Ginn <Dave Ginn>
2016-06-11 16:32:52 -0400

    add existing work

---
+++ CincyAzureNotificationHub/WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Microsoft.ServiceBus;
+using Microsoft.Azure;
+using Microsoft.ServiceBus.Messaging;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post(SimpleModel model)
+        {
+            // Create the queue if it does not exist already.
+            string connectionString =
+                CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+
+            var namespaceManager =
+                NamespaceManager.CreateFromConnectionString(connectionString);
+
+            if (!namespaceManager.QueueExists("TestQueue"))
+            {
+                namespaceManager.CreateQueue("TestQueue");
+            }
+
+            QueueClient Client =
+                QueueClient.CreateFromConnectionString(connectionString, "TestQueue");
+
+            Client.Send(new BrokeredMessage("Hello from cincy azure"));
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CincyAzureNotificationHub/WebJob/Functions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.NotificationHubs;
+
+namespace WebJob
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([ServiceBusTrigger("TestQueue")] string message, TextWriter log)
+        {
+            log.WriteLine(message);
+            SendNotificationAsync().Wait();
+        }
+
+        static async Task SendNotificationAsync()
+        {
+            NotificationHubClient hub = NotificationHubClient
+                .CreateClientFromConnectionString("Endpoint=sb://cincyazure.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=HDCgcWXf9CLsdmgvnNzfSqy66ZRLC++E07yzG1BRxkg=", "CincyAzure");
+
+            var toast = @"<toast><visual><binding template=""ToastText01""><text id=""1"">Hello from a .NET App!</text></binding></visual></toast>";
+            await hub.SendWindowsNativeNotificationAsync(toast);
+        }
+    }
+}
